90fb8f263e8daf7a2d62ce0543d6b0be10b11da4
amalalqoud <AmalAlqoud@live.com>
2021-12-22 22:59:19 +0300

    weather

--- js/news.js
+++ js/news.js
@@ -15,6 +15,9 @@ document.getElementById("searchWordBtn").addEventListener("click", function() {
 console.log(search);
 
 
+
+
+
 const fetchPromise = fetch(`https://newsapi.org/v2/top-headlines?category=${searchWord}&apiKey=2f1bb7244f434f6daa18bf4fc36274d6`);
 fetchPromise.then(response => {
     return response.json();
@@ -33,51 +36,33 @@ fetchPromise.then(response => {
 });
 
 
-//-------side ----check1-------
-document.getElementById("check1").addEventListener("click", function () {
-    let newst = 'publishedAt';
-    document.getElementById('titleID').innerHTML=categories;
-    const fetchPromise = fetch(`https://newsapi.org/v2/everything?domains=techcrunch.com,thenextweb.com&sortBy={newst}&apiKey=2f1bb7244f434f6daa18bf4fc36274d6
-    `);
-    fetchPromise.then(response => {
+//-------side ----weather-------
+
+    const fetchPromiseWeather = fetch(`https://api.weatherbit.io/v2.0/current?lat=35.7796&lon=-78.6382&key=da7e2148cfed42bbaf76dd2251b17c8c&include=minutely`);
+    fetchPromiseWeather.then(response => {
         return response.json();
     }).then(response => {
-        console.log(response.articles);
-        document.getElementById('news').innerHTML = response.articles.map(element =>
-            `<div class="card m-1" style="width: 16rem;">
-    <img src="${element.urlToImage}" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 id="cardTitle" class="card-title">${element.title}</h5>
-      <p id="cardBody" class="card-text">${element.description}</p>
-    </div>
-    <a href="${element.url}" class="card-footer">view detail</a>
-    </div>`).join('');
+        console.log(response.data);
+        document.getElementById('weatherId').innerHTML = response.data.map(element =>
+            `<div class="card">
+            <div class="card-header">
+              <h5>${element.city_name}</h5>
+            </div>
+            <div class="card-body">
+              <h6>${element.weather.description}</h6>
+              <span></span>
+              <h2>${element.temp}°</h2>
+            </div>
+            <div class="card-footer"><h6>${element.datetime}</h6></div>
+          </div>`).join('');
+          
 
     });
-});
+
 
 
 //-----check 2----------------------
-document.getElementById("check2").addEventListener("click", function () {
-    let countries = 'us';
-    document.getElementById('titleID').innerHTML=countries;
-    const fetchPromise = fetch(`https://newsapi.org/v2/top-headlines?country=${countries}&apiKey=2f1bb7244f434f6daa18bf4fc36274d6`);
-    fetchPromise.then(response => {
-        return response.json();
-    }).then(response => {
-        console.log(response.articles);
-        document.getElementById('news').innerHTML = response.articles.map(element =>
-            `<div class="card m-1" style="width: 16rem;">
-    <img src="${element.urlToImage}" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 id="cardTitle" class="card-title">${element.title}</h5>
-      <p id="cardBody" class="card-text">${element.description}</p>
-    </div>
-    <a href="${element.url}" class="card-footer">view detail</a>
-    </div>`).join('');
 
-    });
-});
 
 //-------nav categories----Sports-------
 document.getElementById("sportBtn").addEventListener("click", function () {
