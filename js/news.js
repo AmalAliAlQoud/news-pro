a4c3267c84d247a4667aeed2a88d248f312eedc5
amalalqoud <AmalAlqoud@live.com>
2021-12-24 00:49:35 +0300

    footer

--- js/news.js
+++ js/news.js
@@ -1,8 +1,9 @@
-let searchWord = 'health';
 var search = '';
 let lat;
 let lon;
 
+let categories = 'general';
+reunNews();
 
 //-------side ----weather-------
 if (navigator.geolocation) {
@@ -13,8 +14,6 @@ function showPosition(position) {
     lat = position.coords.latitude;
     lon = position.coords.longitude;
     var fetchPromiseWeather = fetch(`https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=da7e2148cfed42bbaf76dd2251b17c8c&include=minutely`);
-
-    //const fetchPromiseWeather = fetch(`https://api.weatherbit.io/v2.0/current?lat=35.7796&lon=-78.6382&key=da7e2148cfed42bbaf76dd2251b17c8c&include=minutely`);
     fetchPromiseWeather.then(response => {
         return response.json();
     }).then(response => {
@@ -29,20 +28,18 @@ function showPosition(position) {
            <span></span>
            <h2>${element.temp}°</h2>
          </div>
-         <div class="card-footer"><h6>${element.datetime}</h6></div>
+         <div class="card-footer"><small>${element.datetime}</small></div>
        </div>`).join('');
     });
 }
 
 
+
 //---------search-------------------------------
-let categories = 'News';
-document.getElementById('titleID').innerHTML = categories;
-//let S=document.getElementById('searchWordId').innerHTML;
 document.getElementById("searchWordBtn").addEventListener("click", (e) => {
     e.preventDefault();
     search = document.getElementById('searchWordId').value;
-    document.getElementById('titleID').innerHTML = 'your search result about '+search;
+    document.getElementById('titleID').innerHTML = 'your search result about ' + search;
     console.log(search);
     const fetchPromise = fetch(`https://newsapi.org/v2/top-headlines?q=${search}&apiKey=2f1bb7244f434f6daa18bf4fc36274d6`);
     fetchPromise.then(response => {
@@ -63,9 +60,30 @@ document.getElementById("searchWordBtn").addEventListener("click", (e) => {
 
 
 
-//-------nav categories----Sports-------
-document.getElementById("sportBtn").addEventListener("click", function () {
-    let categories = 'sports';
+const navBtn = document.querySelectorAll(".ABC");
+for (let i = 0; i < navBtn.length; i++) {
+    navBtn[i].addEventListener("click", function () {
+        if (i == 0)
+            categories = 'general';
+        if (i == 1)
+            categories = 'health';
+        if (i == 2)
+            categories = 'sports';
+        if (i == 3)
+            categories = 'science';
+        if (i == 4)
+            categories = 'business';
+        if (i == 5)
+            categories = 'entertainment';
+        if (i == 6)
+            categories = 'technology';
+        reunNews();
+
+    });
+}
+
+
+function reunNews() {
     document.getElementById('titleID').innerHTML = categories;
     const fetchPromise = fetch(`https://newsapi.org/v2/top-headlines?category=${categories}&apiKey=2f1bb7244f434f6daa18bf4fc36274d6`);
     fetchPromise.then(response => {
@@ -83,134 +101,5 @@ document.getElementById("sportBtn").addEventListener("click", function () {
     </div>`).join('');
 
     });
-});
-//-----technology--------
-document.getElementById("technologyBtn").addEventListener("click", function () {
-    let categories = 'technology';
-    document.getElementById('titleID').innerHTML = categories;
-    const fetchPromise = fetch(`https://newsapi.org/v2/top-headlines?category=${categories}&apiKey=2f1bb7244f434f6daa18bf4fc36274d6`);
-    fetchPromise.then(response => {
-        return response.json();
-    }).then(response => {
-        console.log(response.articles);
-        document.getElementById('news').innerHTML = response.articles.map(element =>
-            `<div class="card m-1" style="width: 16rem;">
-    <img src="${element.urlToImage}" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 id="cardTitle" class="card-title">${element.title}</h5>
-      <p id="cardBody" class="card-text">${element.description}</p>
-    </div>
-    <a href="${element.url}" class="card-footer">view detail</a>
-    </div>`).join('');
+}
 
-    });
-});
-//
-//-------nav categories----health-------
-document.getElementById("healthBtn").addEventListener("click", function () {
-    let categories = 'health';
-    document.getElementById('titleID').innerHTML = categories;
-    const fetchPromise = fetch(`https://newsapi.org/v2/top-headlines?category=${categories}&apiKey=2f1bb7244f434f6daa18bf4fc36274d6`);
-    fetchPromise.then(response => {
-        return response.json();
-    }).then(response => {
-        console.log(response.articles);
-        document.getElementById('news').innerHTML = response.articles.map(element =>
-            `<div class="card m-1" style="width: 16rem;">
-        <img src="${element.urlToImage}" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 id="cardTitle" class="card-title">${element.title}</h5>
-          <p id="cardBody" class="card-text">${element.description}</p>
-        </div>
-        <a href="${element.url}" class="card-footer">view detail</a>
-        </div>`).join('');
-
-    });
-});
-
-//-------nav categories----general-------
-document.getElementById("generalBtn").addEventListener("click", function () {
-    let categories = 'general';
-    document.getElementById('titleID').innerHTML = categories;
-    const fetchPromise = fetch(`https://newsapi.org/v2/top-headlines?category=${categories}&apiKey=2f1bb7244f434f6daa18bf4fc36274d6`);
-    fetchPromise.then(response => {
-        return response.json();
-    }).then(response => {
-        console.log(response.articles);
-        document.getElementById('news').innerHTML = response.articles.map(element =>
-            `<div class="card m-1" style="width: 16rem;">
-        <img src="${element.urlToImage}" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 id="cardTitle" class="card-title">${element.title}</h5>
-          <p id="cardBody" class="card-text">${element.description}</p>
-        </div>
-        <a href="${element.url}" class="card-footer">view detail</a>
-        </div>`).join('');
-
-    });
-});
-
-//-------nav categories----science-------
-document.getElementById("scienceBtn").addEventListener("click", function () {
-    let categories = 'science';
-    document.getElementById('titleID').innerHTML = categories;
-    const fetchPromise = fetch(`https://newsapi.org/v2/top-headlines?category=${categories}&apiKey=2f1bb7244f434f6daa18bf4fc36274d6`);
-    fetchPromise.then(response => {
-        return response.json();
-    }).then(response => {
-        console.log(response.articles);
-        document.getElementById('news').innerHTML = response.articles.map(element =>
-            `<div class="card m-1" style="width: 16rem;">
-    <img src="${element.urlToImage}" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 id="cardTitle" class="card-title">${element.title}</h5>
-      <p id="cardBody" class="card-text">${element.description}</p>
-    </div>
-    <a href="${element.url}" class="card-footer">view detail</a>
-    </div>`).join('');
-
-    });
-});
-//
-//-------nav categories----entertainment-------
-document.getElementById("entertainmentBtn").addEventListener("click", function () {
-    let categories = 'entertainment';
-    document.getElementById('titleID').innerHTML = categories;
-    const fetchPromise = fetch(`https://newsapi.org/v2/top-headlines?category=${categories}&apiKey=2f1bb7244f434f6daa18bf4fc36274d6`);
-    fetchPromise.then(response => {
-        return response.json();
-    }).then(response => {
-        console.log(response.articles);
-        document.getElementById('news').innerHTML = response.articles.map(element =>
-            `<div class="card m-1" style="width: 16rem;">
-    <img src="${element.urlToImage}" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 id="cardTitle" class="card-title">${element.title}</h5>
-      <p id="cardBody" class="card-text">${element.description}</p>
-    </div>
-    <a href="${element.url}" class="card-footer">view detail</a>
-    </div>`).join('');
-
-    });
-});
-//-------nav categories----business-------
-document.getElementById("businessBtn").addEventListener("click", function () {
-    let categories = 'business';
-    document.getElementById('titleID').innerHTML = categories;
-    const fetchPromise = fetch(`https://newsapi.org/v2/top-headlines?category=${categories}&apiKey=2f1bb7244f434f6daa18bf4fc36274d6`);
-    fetchPromise.then(response => {
-        return response.json();
-    }).then(response => {
-        console.log(response.articles);
-        document.getElementById('news').innerHTML = response.articles.map(element =>
-            `<div class="card m-1" style="width: 16rem;">
-    <img src="${element.urlToImage}" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 id="cardTitle" class="card-title">${element.title}</h5>
-      <p id="cardBody" class="card-text">${element.description}</p>
-    </div>
-    <a href="${element.url}" class="card-footer">view detail</a>
-    </div>`).join('');
-
-    });
-});
